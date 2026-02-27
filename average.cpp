1a3335a342e8eca2adc7cd980a2c57bb2eb2697a
ZZMolotov <zzmolotov8559@gmail.com>
2026-02-27 15:18:27 +0700

    Lab22_1 fixed

--- average.cpp
+++ average.cpp
@@ -1,1 +1,20 @@
+#include<iostream>
+#include<cstdlib>
+using namespace std;
 
+int main(int arg, char* argv[]){
+    float sum = 0;
+    
+    for(int i = 1; i < arg; i++){
+        sum += atof(argv[i]);
+    }
+    float avg = sum/(arg-1);
+    if(arg < 2){
+        cout<<"Please input numbers to find average."<<endl;
+    }else{
+    cout<<"---------------------------------"<<endl;
+    cout<<"Average of "<<arg-1<<" numbers = "<<avg<<endl;
+    cout<<"---------------------------------"<<endl;
+    }
+
+}
